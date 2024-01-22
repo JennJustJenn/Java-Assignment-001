ac1a91c0a7ac2b6f571d92c21c8cf758924034ea
JennJustJenn <jgibson967@mycr.redwoods.edu>
2024-01-21 19:22:03 -0800

    Assignment001_Spring2024

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,21 @@
+/**
+ *
+ *@author Trevor Hartman
+ *@author Jennifer Gibson
+ * Date 1-21-2024
+ *
+ * @Since Version 1.0
+ *
+ */
+
+
+
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 
 }
